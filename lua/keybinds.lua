c3c59318a0b69f73328098ef4d08ca61f39660e6
phamhiep2506 <phamhiep25062k@gmail.com>
2022-05-06 14:02:55 +0700

    update which key

--- lua/keybinds.lua
+++ lua/keybinds.lua
@@ -1,30 +1,5 @@
--- disable 'Space' key
-vim.api.nvim_set_keymap('n', '<Space>', '', { noremap = true })
-
 -- set 'Space' key as leader key
 vim.g.mapleader = ' '
-
--- disable arrow keys
-vim.api.nvim_set_keymap('', '<Up>', '', { noremap = true })
-vim.api.nvim_set_keymap('i', '<Up>', '', { noremap = true })
-vim.api.nvim_set_keymap('', '<Down>', '', { noremap = true })
-vim.api.nvim_set_keymap('i', '<Down>', '', { noremap = true })
-vim.api.nvim_set_keymap('', '<Left>', '', { noremap = true })
-vim.api.nvim_set_keymap('i', '<Left>', '', { noremap = true })
-vim.api.nvim_set_keymap('', '<Right>', '', { noremap = true })
-vim.api.nvim_set_keymap('i', '<Right>', '', { noremap = true })
-
--- move bettween windows
-vim.api.nvim_set_keymap('n', '<C-h>', '<C-w>h', { noremap = true })
-vim.api.nvim_set_keymap('n', '<C-j>', '<C-w>j', { noremap = true })
-vim.api.nvim_set_keymap('n', '<C-k>', '<C-w>k', { noremap = true })
-vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l', { noremap = true })
-
--- resize window
-vim.api.nvim_set_keymap('n', '<A-h>', ':vertical resize -2<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<A-j>', ':resize +2<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<A-k>', ':resize -2<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<A-l>', ':vertical resize +2<CR>', { noremap = true })
 
 --  move block of text
 vim.api.nvim_set_keymap('v', '>', '>gv', { noremap = true })
--- lua/nv-whichkey.lua
+++ lua/nv-whichkey.lua
@@ -13,5 +13,19 @@ wk.register({
     v = {"<cmd>split<CR>", "Split Vertical"},
     h = {"<cmd>vsplit<CR>", "Split Horizontal"}
   },
+  w = {
+    name = "Window",
+    h = {"<C-w>h", "Select Window Left"},
+    j = {"<C-w>j", "Select Window Down"},
+    k = {"<C-w>k", "Select Window Up"},
+    l = {"<C-w>l", "Select Window Right"},
+    r = {
+      name = "Resize Window",
+      ["1"] = {"<cmd>resize +5<CR>", "Resize Vertical +5"},
+      ["2"] = {"<cmd>resize -5<CR>", "Resize Vertical -5"},
+      ["3"] = {"<cmd>vertical resize +5<CR>", "Resize Horizontal +5"},
+      ["4"] = {"<cmd>vertical resize -5<CR>", "Resize Horizontal -5"},
+    }
+  }
 }, { prefix = "<leader>" })
 
