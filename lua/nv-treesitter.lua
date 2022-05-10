f77f784908337f5a8fadcfdc5d429f8fbddf0c30
phamhiep2506 <phamhiep25062k@gmail.com>
2022-05-10 07:36:13 +0700

    update config treesitter

--- lua/nv-treesitter.lua
+++ lua/nv-treesitter.lua
@@ -1,17 +1,4 @@
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = {
-    "html",
-    "css",
-    "scss",
-    "javascript",
-    "typescript",
-    "json",
-    "c",
-    "cpp",
-    "php",
-    "lua",
-  },
-
   highlight = {
     enable = true,
   },
