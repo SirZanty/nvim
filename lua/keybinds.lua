b67def56c4674cbece3ef41f6f97b4da8d2199ac
phamhiep2506 <phamhiep25062k@gmail.com>
2022-05-18 10:05:39 +0700

    update config

--- lua/keybinds.lua
+++ lua/keybinds.lua
@@ -30,6 +30,17 @@ vim.api.nvim_set_keymap('n', '<A-j>', ':resize +2<CR>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<A-k>', ':resize -2<CR>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<A-l>', ':vertical resize -2<CR>', { noremap = true })
 
+-- buffers :bnext and :bprevious
+vim.api.nvim_set_keymap('n', '<A-.>', ':BufferLineCycleNext<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<A-,>', ':BufferLineCyclePrev<CR>', { noremap = true, silent = true })
+
+-- move the current buffer backwards or forwards
+vim.api.nvim_set_keymap('n', '<A->>', ':BufferLineMoveNext<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<A-<>', ':BufferLineMovePrev<CR>', { noremap = true, silent = true })
+
+-- close buffer
+vim.api.nvim_set_keymap('n', '<A-c>', ':BufferLinePickClose<CR>', { noremap = true, silent = true })
+
 -- save file
 vim.api.nvim_set_keymap('n', '<C-s>', ':update<CR>', { noremap = true })
 
