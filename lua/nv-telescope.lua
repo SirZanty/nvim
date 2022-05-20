ef6263030c62c283585fe90b9b81161210a00706
phamhiep2506 <phamhiep25062k@gmail.com>
2022-05-20 13:22:53 +0700

    update config telescope

--- lua/nv-telescope.lua
+++ lua/nv-telescope.lua
@@ -4,4 +4,4 @@ require('telescope').setup{
   }
 }
 
-vim.api.nvim_set_keymap('n', '<leader>ff', ':Telescope find_files<CR>', { noremap = true, silent=true })
+vim.api.nvim_set_keymap('n', '<C-p>', ':Telescope find_files<CR>', { noremap = true, silent=true })
--- lua/nv-tree.lua
+++ lua/nv-tree.lua
@@ -23,4 +23,4 @@ require'nvim-tree'.setup{
   },
 }
 
-vim.api.nvim_set_keymap('n', '<C-n>', ':NvimTreeToggle<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-b>', ':NvimTreeToggle<CR>', { noremap = true })
