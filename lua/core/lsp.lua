80945628d52e0011fcd4c20e37369d6fede692dd
phamhiep2506 <phamhiep25062k@gmail.com>
2022-09-23 08:53:24 +0700

    🚀 change lsp dig

--- lua/core/lsp.lua
+++ lua/core/lsp.lua
@@ -115,8 +115,8 @@ vim.diagnostic.config({
     virtual_text = true,
     signs = true,
     underline = true,
-    update_in_insert = true,
-    severity_sort = true,
+    update_in_insert = false,
+    severity_sort = false,
 })
 
 vim.diagnostic.config({
