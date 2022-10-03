bbf9e2deaf4560159e95b3a6348d890430f122b8
phamhiep2506 <phamhiep25062k@gmail.com>
2022-10-03 23:50:31 +0700

    change icon telescope

--- lua/core/telescope.lua
+++ lua/core/telescope.lua
@@ -1,5 +1,7 @@
 require('telescope').setup{
     defaults = {
+        prompt_prefix = ' ',
+        selection_caret = ' ',
         file_ignore_patterns = {'node_modules', '.git', 'vendor', 'obj', 'bin'}
     }
 }
